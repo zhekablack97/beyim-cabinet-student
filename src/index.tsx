90d404676e302d34567d131cd9d74cbc375b2489
Евгений Морозов <evgenijmorozov@MacBook-Air-Irina.local>
2023-12-12 15:02:59 +0700

    add store

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
 
-
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
 );
 
--- src/store.ts
+++ src/store.ts
@@ -5,6 +5,8 @@ const store = configureStore({
     reducer: {},
 });
 
+export default store;
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
