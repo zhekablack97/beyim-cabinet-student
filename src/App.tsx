d7ce6735fe9c89dcddeb6ad930336d96f3509214
Евгений Морозов <evgenijmorozov@MacBook-Air-Irina.local>
2023-12-12 14:39:24 +0700

    lang

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import logo from './logo.svg';
 import './App.scss';
 
+const Tittle: React.FC = () => {
+    const { t } = useTranslation('translation');
+
+    return <h1>{t('authorization.title')}</h1>;
+};
+
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Suspense fallback="...loading">
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <Tittle />
+                    <p>aa</p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        </Suspense>
     );
 }
 
