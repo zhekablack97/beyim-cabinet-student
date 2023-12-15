88dad69fd323c2ec28f89e7f843bbe4da6120304
Евгений Морозов <evgenijmorozov@MacBook-Air-Irina.local>
2023-12-15 16:03:36 +0700

    add logo

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,14 +1,21 @@
+import { Link } from 'react-router-dom';
 import { ChangingLanguage } from '../ChangingLanguage';
 
 export const Header: React.FC = () => {
     return (
-        <header>
-            <div>лого</div>
-            <div>Изучить</div>
-            <div>поиск</div>
-            <ChangingLanguage />
-            <div>пожаловаться</div>
-            <div>Профаил</div>
+        <header className="h-14 flex px-6 items-center">
+            <div>
+                <Link to="/">
+                    <img src="/icons/logoMain.svg" alt="" />
+                </Link>
+            </div>
+            <div className="ml-[77px]">Изучить</div>
+            <div className="ml-7">поиск</div>
+            <div className="ml-auto flex items-center">
+                <ChangingLanguage />
+                <div>пожаловаться</div>
+                <div>Профаил</div>
+            </div>
         </header>
     );
 };
